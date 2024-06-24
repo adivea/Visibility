19b8308cbfde323c17b36ba9b6b078d7002fc6d4
adivea <adela@fedarch.org>
2024-06-24 16:07:30 +0200

    creating maps and figures for pub

---
+++ 04_Maps.R
@@ -0,0 +1,16 @@
+e <- drawExtent(show = T, col = "blue")
+plot(Y35); plot(region$geometry, add =T)
+b <- st_make_grid(st_bbox(Y_buf25), n =1)
+c <- st_make_grid(st_bbox(region), n =1)
+
+d <- st_make_grid(st_bbox(st_buffer(region, 5000)), n =1)
+plot(Y35); plot(b, add =T); plot(c, borders = "red", add =T); plot(region$geometry, add =T)
+
+plot(Y);plot(b, add =T); plot(c, borders = "red", add =T); plot(region$geometry, add =T)
+
+dem_sm <- crop(Y, b)
+dem_Y5 <- crop(Y, d)
+dem_Y <- crop(Y, c)
+plot(dem_sm); plot(region$geometry, add =T)
+plot(dem_Y5, col = grey.colors(6, start = 1, end = 0)); plot(region$geometry, add =T); plot(Yam_mnds$geometry, cex= 0.5, add =T)
+writeRaster(dem_Y5, "data/large/Y_dem5.tif")
